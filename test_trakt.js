df2f475e5fd8758746a238a9ad1cfcf567a97d67
Jater-exe <jter6868@gmail.com>
2026-04-25 11:37:26 +0200

    All in

--- test_trakt.js
+++ test_trakt.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const CLIENT_ID = "TRAKT_CLIENT_ID";
+const CLIENT_ID = "09416659f45334451c291b63fc404c05942b5bd23fbd277b1ca41bf196cc6263";
 
 async function getWatchedMovies(username) {
     try {
@@ -21,4 +21,66 @@ async function getWatchedMovies(username) {
     }
 }
 
-getWatchedMovies("sean");
+async function getWatchedShows(username) {
+    try {
+        const res = await axios.get(
+            `https://api.trakt.tv/users/${username}/watched/shows`,
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    "trakt-api-version": "2",
+                    "trakt-api-key": CLIENT_ID
+                }
+            }
+        );
+
+        console.log(res.data);
+    } catch (error) {
+        console.error(error.response?.data || error.message);
+    }
+}
+
+
+async function getRatingMovies(username) {
+    try {
+        const res = await axios.get(
+            `https://api.trakt.tv/users/${username}/ratings/movies`,
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    "trakt-api-version": "2",
+                    "trakt-api-key": CLIENT_ID
+                }
+            }
+        );
+
+        console.log(res.data);
+    } catch (error) {
+        console.error(error.response?.data || error.message);
+    }
+}
+
+async function getRatingShows(username) {
+    try {
+        const res = await axios.get(
+            `https://api.trakt.tv/users/${username}/ratings/shows`,
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    "trakt-api-version": "2",
+                    "trakt-api-key": CLIENT_ID
+                }
+            }
+        );
+
+        console.log(res.data);
+    } catch (error) {
+        console.error(error.response?.data || error.message);
+    }
+}
+
+
+
+var rated_movies = getRatingMovies("sean");
+var rated_shows = getRatingShows("sean");
+rated_movies.
