52a12a1f1faae8ede97c150925f3ce0decd839eb
luciagonzalezzz <lucia.gonzalez.estrada@estudiantat.upc.edu>
2026-04-25 11:47:55 +0200

    5

--- test_trakt.js
+++ test_trakt.js
@@ -2,45 +2,6 @@ const axios = require("axios");
 
 const CLIENT_ID = "09416659f45334451c291b63fc404c05942b5bd23fbd277b1ca41bf196cc6263";
 
-async function getWatchedMovies(username) {
-    try {
-        const res = await axios.get(
-            `https://api.trakt.tv/users/${username}/watched/movies`,
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                    "trakt-api-version": "2",
-                    "trakt-api-key": CLIENT_ID
-                }
-            }
-        );
-
-        console.log(res.data);
-    } catch (error) {
-        console.error(error.response?.data || error.message);
-    }
-}
-
-async function getWatchedShows(username) {
-    try {
-        const res = await axios.get(
-            `https://api.trakt.tv/users/${username}/watched/shows`,
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                    "trakt-api-version": "2",
-                    "trakt-api-key": CLIENT_ID
-                }
-            }
-        );
-
-        console.log(res.data);
-    } catch (error) {
-        console.error(error.response?.data || error.message);
-    }
-}
-
-
 async function getRatingMovies(username) {
     try {
         const res = await axios.get(
@@ -54,9 +15,12 @@ async function getRatingMovies(username) {
             }
         );
 
-        console.log(res.data);
+        return res.data.filter(item => item.rating >= 7);
+
     } catch (error) {
-        console.error(error.response?.data || error.message);
+        console.error(
+          (error.response && error.response.data) || error.message
+        );
     }
 }
 
@@ -73,14 +37,32 @@ async function getRatingShows(username) {
             }
         );
 
-        console.log(res.data);
+        return res.data.filter(item => item.rating >= 7);
+
+
     } catch (error) {
-        console.error(error.response?.data || error.message);
+        console.error(
+          (error.response && error.response.data) || error.message
+        );
     }
 }
 
+async function main() {
+    const username = "sean";
 
+    const rated_movies = await getRatingMovies(username);
+    const rated_shows = await getRatingShows(username);
 
-var rated_movies = getRatingMovies("sean");
-var rated_shows = getRatingShows("sean");
-rated_movies.
+    console.log("Movies >= 7:");
+    console.log(rated_movies);
+
+    console.log("\nShows >= 7:");
+    console.log(rated_shows);
+
+    const topContent = [...rated_movies, ...rated_shows];
+
+    console.log("\nTop content:");
+    console.log(topContent);
+}
+
+main();
